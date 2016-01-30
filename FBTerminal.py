b869a9b2fbe0d1663f085513c789de6666f1bf5a
aneeshdurg <durg2@illinois.edu>
2016-01-29 23:58:01 -0600

    FBTerminal now deletes previous posts

--- FBTerminal.py
+++ FBTerminal.py
@@ -1,10 +1,11 @@
-from os import system
+from os import system, chdir
 from facepy import GraphAPI 
 from subprocess import Popen, PIPE
 graph = GraphAPI('CAAYGdFK3zvYBAOJOgQ9kXQz81DQirsPod1yuljgJlftsETgSW3RWa9hOTzyVeZCXfK0ZCZAZBu6usP9d3mnFUbZAuvq4UGc0PGFsZBHHnej6qhmyDnqZBwchvYbySFLKfzCLNXIRmi5KVZB0yX5zb4wxKQZBVngQEXZAhKef8kZAVQVSImlhy0GGAZBVeexZAnCuwGCL75RsHZA1nnMgZDZD')
 a = graph.get('me/feed')
 last = a['data'][0]['message']
 fileCreated = False
+postedID = None
 while True:
     if fileCreated:
         system('del output.txt')
@@ -16,6 +17,8 @@ while True:
         if cmd[1:]=='exit':
             exit()
         if cmd[1:]=='start-jrepl':
+            if postedID is not None:
+                graph.delete(postedID)
             print "Starting Jrepl"
             last = cmd
             p = Popen(["jrepl"], shell=True, stdin=PIPE, stdout=PIPE)
@@ -27,6 +30,7 @@ while True:
                     p.stdin.write('exit'+'\n')
                     last = ':'+jcmd
                     print "Stopping Jrepl"
+                    postedID = None
                     break
                 elif jcmd!='' and jcmd!=last and jcmd[0]==':':
                     jcmd = jcmd[1:]
@@ -41,11 +45,14 @@ while True:
                         if response!='':
                             toPost+="\n"+response
                     print toPost
-                    graph.post('me/feed', message=toPost)
+                    if postedID is not None:
+                        graph.delete(postedID)
+                    posted = graph.post('me/feed', message=toPost)
+                    postedID = posted['id']
                 last = jcmd
         else:
-            if cmd[1:3]=='cd':
-                os.chdir(cmd[4:])
+            if cmd[1:3]=='cd' and len(cmd)>3:
+                chdir(cmd[4:])
                 fileCreated = False
             else:
                 # if cmd[0:5] == 'echo' or cmd[0:4] == 'dir':
@@ -53,7 +60,11 @@ while True:
                 if cmd[1:5] == 'echo':
                    # print cmd
                    pass
-                system(cmd[1:]+'> output.txt')
+                if '>' in cmd:
+                    fileCreated = False
+                    system(cmd[1:])
+                else:
+                    system(cmd[1:]+'> output.txt')
                 print cmd
             last = cmd
     if fileCreated:
@@ -62,5 +73,7 @@ while True:
         outputFile.close()
         if len(output)!=0:
             print output
-            graph.post('me/feed', message=output)
-
+            if postedID is not None:
+                graph.delete(postedID)
+            posted = graph.post('me/feed', message=output)
+            postedID = posted['id']
