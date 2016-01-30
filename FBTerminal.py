042a7a93550bfbba03633928511797bb24828e69
aneeshdurg <durg2@illinois.edu>
2016-01-30 00:45:48 -0600

    Added security, now require inital unlock and can be locked again

--- FBTerminal.py
+++ FBTerminal.py
@@ -1,22 +1,60 @@
 from os import system, chdir
 from facepy import GraphAPI 
 from subprocess import Popen, PIPE
-graph = GraphAPI('CAAYGdFK3zvYBAOJOgQ9kXQz81DQirsPod1yuljgJlftsETgSW3RWa9hOTzyVeZCXfK0ZCZAZBu6usP9d3mnFUbZAuvq4UGc0PGFsZBHHnej6qhmyDnqZBwchvYbySFLKfzCLNXIRmi5KVZB0yX5zb4wxKQZBVngQEXZAhKef8kZAVQVSImlhy0GGAZBVeexZAnCuwGCL75RsHZA1nnMgZDZD')
+from random import randint
+graph = GraphAPI('CAAYGdFK3zvYBAHCZBpzVbXrztWklY8a63W8qEDZB2eIndk7ZAsh4AtL2EbVtZCIgduuKum1adO91qKanraX9wjX7fbscOAohZCpBlu7lsjdwZAff5HtfWWcUbpl6BxhPGQGOfhmRoU6FNNbycq61KduS9xFzKO6uLYYAsMRloOocSeC7KTGKMV5PvGYvoEzibwMUu0qfp45AZDZD')
 a = graph.get('me/feed')
 last = a['data'][0]['message']
 fileCreated = False
 postedID = None
+locked = True
+key = "#"
+for i in xrange(5):
+    key+=str(randint(0, 9))
+print "Please post the following to unlock FBTerminal: "+key
 while True:
+    if locked:
+        a = graph.get('me/feed')
+        cmd = a['data'][0]['message']
+        if cmd == key:
+            print 'unlocked'
+            if postedID is not None:
+                graph.delete(postedID)
+            locked = False
+            posted = graph.post('me/feed', message='FBTerminal was successfully unlocked!')
+            postedID = posted['id']
+            continue
+        elif cmd==':printKey':
+            print key
+        else:
+            continue
+
     if fileCreated:
         system('del output.txt')
         fileCreated = False
+    
     a = graph.get('me/feed')
     cmd = a['data'][0]['message']
+    
     fileCreated = False    
     if cmd[0] == ':' and cmd!=last:
         if cmd[1:]=='exit':
+            if postedID is not None:
+                graph.delete(postedID)
             exit()
-        if cmd[1:]=='start-jrepl':
+        elif cmd[1:]=='lockFBTerm':
+            print 'locked'
+            key = "#"
+            for i in xrange(5):
+                key+=str(randint(0, 9))
+            print "Please post the following to unlock FBTerminal: "+key
+            locked = True
+            if postedID is not None:
+                graph.delete(postedID)
+                postedID = None
+            posted = graph.post('me/feed', message="FBTerminal is locked!")
+            postedID = posted['id']
+        elif cmd[1:]=='start-jrepl':
             if postedID is not None:
                 graph.delete(postedID)
             print "Starting Jrepl"
